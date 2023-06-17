6fb7fd808d9da5d616402252954c055e7a5ad15d
Magnus Kolstad <mrkolby@gmail.com>
2023-06-17 21:13:50 +0200

    Add form types

---
+++ src/lib/types.ts
@@ -0,0 +1,39 @@
+export type FormColorType =
+  | 'blue'
+  | 'red'
+  | 'green'
+  | 'purple'
+  | 'teal'
+  | 'yellow'
+  | 'orange'
+  | 'primary'
+  | 'secondary';
+
+export declare type FormSizeType = typeof sm | typeof md | typeof lg;
+
+export type InputType =
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'reset'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week'
+  | 'search';
+
+export type SelectOptionType = {
+  name: string | number;
+  value: string | number;
+};
+
+export declare type SizeType = typeof xs | typeof sm | typeof md | typeof lg | typeof xl;
