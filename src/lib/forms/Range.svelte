c015476c436c86bdde388db5a18e227c58fc9b4d
Magnus Kolstad <mrkolby@gmail.com>
2023-06-17 22:49:12 +0200

    components/forms
    * Add form types
    
    * Add forms components
    
    # Conflicts:
    #	src/lib/types.ts
    
    * Fix types path

---
+++ src/lib/forms/Range.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { cn } from '$lib/helpers/cn';
+
+  export let value: number;
+  export let size: 'sm' | 'md' | 'lg' = 'md';
+
+  const sizes = {
+    sm: 'h-1 range-sm',
+    md: 'h-2',
+    lg: 'h-3 range-lg',
+  };
+  let inputClass: string;
+  $: inputClass = cn(
+    'w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700',
+    sizes[size] ?? sizes.md,
+    $$props.class
+  );
+</script>
+
+<input
+  type="range"
+  bind:value
+  {...$$restProps}
+  class={inputClass}
+  on:change
+  on:click
+  on:keydown
+  on:keypress
+  on:keyup
+/>
