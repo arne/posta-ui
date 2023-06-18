c3c71b8e2a1ae3d556892f262bdef4dcb718d9cc
Magnus Kolstad <mrkolby@gmail.com>
2023-06-18 13:11:41 +0200

    Each time we meet love

--- src/lib/tabs/Tabs.svelte
+++ src/lib/tabs/Tabs.svelte
@@ -26,7 +26,7 @@
     full: 'p-4 w-full group-first:rounded-l-lg group-last:rounded-r-lg text-gray-900 bg-gray-100 focus:ring-4 focus:ring-primary-300 focus:outline-none dark:bg-gray-700 dark:text-white',
     pill: 'py-3 px-4 text-white bg-primary-600 rounded-lg',
     underline:
-      'p-4 text-primary-600 border-b-2 border-red-600 dark:text-primary-500 dark:border-red-500',
+      'py-2 px-4 text-primary-600 border-b-2 border-posta-red dark:text-primary-500 dark:border-posta-red',
     none: '',
   };
 
@@ -34,7 +34,7 @@
     full: 'p-4 w-full group-first:rounded-l-lg group-last:rounded-r-lg text-gray-500 dark:text-gray-400 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-primary-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700',
     pill: 'py-3 px-4 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white',
     underline:
-      'p-4 border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-gray-500 dark:text-gray-400',
+      'py-2 px-4 border-b-2 border-transparent hover:text-black hover:border-black dark:hover:text-gray-300 text-gray-500 dark:text-gray-400',
     none: '',
   };
 
--- src/routes/u/[person]/+page.svelte
+++ src/routes/u/[person]/+page.svelte
@@ -2,92 +2,113 @@
   import Post from '$lib/Post.svelte';
   import SingleComment from '$lib/Comments/SingleComment.svelte';
   import { Tabs, TabItem } from '$lib/tabs';
-  import SvelteMarkdown from 'svelte-markdown';
-  import { Globe, MessagesSquare, User, SquareStack } from 'lucide-svelte';
+  import Badge from '$lib/Badge.svelte';
 
+  import SvelteMarkdown from 'svelte-markdown';
+  import { Cake, Globe, MessagesSquare, SquareStack } from 'lucide-svelte';
+
+  import dayjs from 'dayjs';
+  import relativeTime from 'dayjs/plugin/relativeTime.js';
+  dayjs.extend(relativeTime);
 
   export let data;
-  const [person, posts] = [data.person_view.person, data.posts];
+  console.log(data);
+  const [person, comments, counts, posts] = [
+    data.person_view.person,
+    data.comments,
+    data.person_view.counts,
+    data.posts,
+  ];
 </script>
 
-<div class="flex gap-6">
-  <main class="flex-auto w-64">
+<div class="flex flex-col gap-6 md:flex-row">
+  <main class="flex-auto md:w-64 order-2 md:order-1">
     <Tabs style="underline">
       <TabItem open>
         <div slot="title" class="flex items-center gap-2">
-          <Globe class="h-6 w-6" />
+          <Globe class="h-4 w-4" />
           Overview
         </div>
         <p class="text-sm text-gray-500 dark:text-gray-400">Overview</p>
       </TabItem>
       <TabItem>
         <div slot="title" class="flex items-center gap-2">
-          <SquareStack class="h-6 w-6" />
+          <SquareStack class="h-4 w-4" />
           Posts
         </div>
-        {#each data.posts as post}
+        {#each posts as post}
           <Post {post} link />
         {/each}
       </TabItem>
       <TabItem>
         <div slot="title" class="flex items-center gap-2">
-          <MessagesSquare class="h-6 w-6" />
+          <MessagesSquare class="h-4 w-4" />
           Comments
         </div>
-        {#each data.comments as comment}
+        {#each comments as comment}
           <div id={`#${comment.comment.id}`} class="mb-4 pb-2 border-b">
             <h3 class="font-bold mb-3">{comment.post.name}</h3>
             <SingleComment {comment} />
           </div>
         {/each}
       </TabItem>
-      <TabItem>
-        <div slot="title" class="flex items-center gap-2">
-          <User class="h-6 w-6" />
-          Profile
-        </div>
-        <p class="text-sm text-gray-500 dark:text-gray-400">Profile</p>
-      </TabItem>
     </Tabs>
   </main>
 
-  <section class="w-80">
-    <div class="h-28 overflow-hidden">
+  <section class="md:w-80 order-1 md:order-2">
+    <div class="h-12 sm:h-32 overflow-hidden">
       {#if person.banner}
         <img src={person.banner} alt="Banner" class="w-full rounded-tl-md rounded-tr-md" />
       {:else}
-        <div class="bg-yellow-100 h-28 w-full rounded-tl-md rounded-tr-md" />
+        <div class="bg-chill-army-green h-32 w-full rounded-tl-md rounded-tr-md" />
       {/if}
     </div>
     <div class="relative">
       <div
-        class="w-20 h-20 sm:w-32 sm:h-32 bg-white rounded-full absolute sm:-top-16 -top-8 sm:left-4 left-2"
+        class="w-20 h-20 sm:w-24 sm:h-24 bg-white rounded-full absolute sm:-top-10 -top-6 sm:left-2 left-2"
       >
         {#if person.avatar}
           <img
             src={person.avatar}
-            class="w-20 h-20 sm:w-32 sm:h-32 rounded-full text-left p-1"
+            class="w-20 h-20 sm:w-24 sm:h-24 rounded-full text-left p-0.5"
             alt={person.name}
           />
         {:else}
-          <div class="bg-red-100 w-full h-full rounded-full" />
+          <div class="bg-blight-blue w-full h-full rounded-full" />
         {/if}
       </div>
     </div>
 
-    <div class="border border-gray-200 border-t-0 rounded-bl-md rounded-br-md p-2 pt-18">
-      <div class="text-2xl font-bold">
+    <div class="border border-gray-200 border-t-0 rounded-bl-md rounded-br-md p-2 pt-16">
+      <h4 class="text-xl font-medium">
         {person.display_name || person.name}
+      </h4>
+
+      <div class="flex gap-2">
+        <div class="text-posta-red text-sm">
+          @{person.name}
+        </div>
+
+        {#if person.admin}
+          <Badge color="dark" class="font-bold">admin</Badge>
+        {/if}
       </div>
-      @{person.name}
 
       {#if person.bio}
-        <div class="prose"><SvelteMarkdown source={person.bio} /></div>
+        <div class="mt-3"><SvelteMarkdown source={person.bio} /></div>
       {/if}
 
-      <ul>
-        <li>{person.published}</li>
-      </ul>
+      <div class="my-3 flex gap-2">
+        <Badge color="dark" class="font-bold">{`${counts.post_count} posts`}</Badge>
+        <Badge color="dark" class="font-bold">{`${counts.comment_count} comments`}</Badge>
+      </div>
+
+      <div>{`Joined ${dayjs().to(dayjs(person.published))}`}</div>
+
+      <div class="flex items-center gap-1">
+        <Cake class="w-5 h-5 hover:text-yellow-700" />
+        {`Cake day: ${dayjs(person.published).format('MMM DD, YYYY')}`}
+      </div>
     </div>
   </section>
 </div>
