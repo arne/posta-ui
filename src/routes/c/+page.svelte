fb6b5bcebb956a322e05365ece2821aac0aa53a5
Magnus Kolstad <mrkolby@gmail.com>
2023-06-26 09:11:57 +0200

    Trying an updated community list view
    Start support for list view aswell

--- src/routes/c/+page.svelte
+++ src/routes/c/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   export let data;
 
+  import { AlignJustify, LayoutGrid } from 'lucide-svelte';
+
+  import Badge from '$lib/Badge.svelte';
+  import { Button } from '$lib/button';
+
   import dayjs from 'dayjs';
   import relativeTime from 'dayjs/plugin/relativeTime.js';
   dayjs.extend(relativeTime);
@@ -13,58 +18,82 @@
 </svelte:head>
 
 <div>
-  <ul class="grid grid-cols-1 gap-x-6 gap-y-6 lg:grid-cols-4 xl:gap-x-4">
+  <div class="flex justify-between py-2 mb-4 items-center border-b">
+    <h1 class="text-xl">{`${data.communities.length} Posta communities`}</h1>
+    <div>
+      <Button variant="icon" size="icon">
+        <AlignJustify class="h-6 w-6" />
+      </Button>
+      <Button variant="icon" size="icon">
+        <LayoutGrid class="h-6 w-6" />
+      </Button>
+    </div>
+  </div>
+
+  <ul class="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-4">
     {#each data.communities as community}
       <li class="overflow-hidden rounded-md border">
         <a href={getLocalUrl(community.community)}>
-          <div
-            class="flex items-center gap-x-4 border-b bg-gray-50 dark:bg-gray-700 p-3 bg-[url('{community
-              .community.banner}')]"
-          >
-            <img
-              src={community.community.icon}
-              alt={community.community.title}
-              class="inline-block h-10 w-10 rounded-full"
-            />
-            <div>
-              <div class="text-sm font-medium leading-6 text-gray-900">
-                {community.community.title}
-              </div>
-              <div class="text-xs">
+          <div class="h-14 sm:h-28 overflow-hidden">
+            {#if community.community.banner}
+              <img
+                src={`${community.community.banner}?format=webp&thumbnail=512`}
+                alt="Banner"
+                class="rounded-tl-md rounded-tr-md w-full h-full object-cover"
+              />
+            {:else}
+              <div class="bg-blight-blue h-28 w-full rounded-tl-md rounded-tr-md" />
+            {/if}
+          </div>
+
+          <div class="relative">
+            <div
+              class="w-14 h-14 bg-white rounded-full absolute sm:-top-10 -top-6 sm:left-2 left-2"
+            >
+              {#if community.community.icon}
+                <img
+                  src={community.community.icon}
+                  alt={community.community.title}
+                  class="inline-block h-14 w-14 rounded-full p-0.5"
+                />
+              {:else}
+                <div class="bg-chill-army-green w-full h-full rounded-full" />
+              {/if}
+            </div>
+          </div>
+
+          <div class="p-2 pt-10 sm:pt-6">
+            <h4 class="text-md font-medium">
+              {community.community.title}
+            </h4>
+
+            <div class="flex gap-2">
+              <div class="text-posta-red text-sm">
                 !{getLocalUrl(community.community).substring(3)}
               </div>
             </div>
+
+            <div class="my-3 gap-2">
+              <Badge color="dark" class="font-bold">{`${community.counts.posts} posts`}</Badge>
+              <Badge color="dark" class="font-bold"
+                >{`${community.counts.subscribers} subscribers`}</Badge
+              >
+              <Badge color="dark" class="font-bold"
+                >{`${community.counts.users_active_month} users/month`}</Badge
+              >
+            </div>
+
+            <div>
+              <time datetime={community.community.updated}>
+                {`Updated ${
+                  community.community.updated
+                    ? dayjs().to(dayjs(community.community.updated))
+                    : dayjs().to(dayjs(community.community.published))
+                }`}
+              </time>
+            </div>
           </div>
         </a>
-        <dl class="-my-3 divide-y divide-gray-200 px-2 py-4 text-sm leading-6">
-          <div class="flex justify-between gap-x-4 py-1">
-            <dt class="text-gray-500">Updated</dt>
-            <dd class="text-gray-700">
-              <time datetime={community.community.updated}
-                >{community.community.updated
-                  ? dayjs().to(dayjs(community.community.updated))
-                  : dayjs().to(dayjs(community.community.published))}</time
-              >
-            </dd>
-          </div>
-
-          <div class="flex justify-between gap-x-4 py-1">
-            <dt class="text-gray-500">Subscribers</dt>
-            <dd class="flex items-start gap-x-2">
-              <div class="font-medium text-gray-900">
-                {community.subscribed === 'Subscribed'}
-                {community.counts.subscribers}
-              </div>
-            </dd>
-          </div>
-
-          <div class="flex justify-between gap-x-4 py-1">
-            <dt class="text-gray-500">Posts</dt>
-            <dd class="flex items-start gap-x-2">
-              <div class="font-medium text-gray-900">{community.counts.posts}</div>
-            </dd>
-          </div>
-        </dl>
       </li>
     {/each}
   </ul>
